b2ac7dbe680e386942aa438581292db2efecc66f
monkeycoderw <3897177+monkeycoderw@users.noreply.github.com>
2019-08-01 23:46:10 +0800

    横向列表

--- lib/demo/MyList.dart
+++ lib/demo/MyList.dart
@@ -12,24 +12,39 @@ class MyApp extends StatelessWidget {
         appBar: new AppBar(
           title: new Text("Lis View Widget")
         ),
-        body: new ListView(
-          children: <Widget>[
-            new ListTile(
-              leading: new Icon(Icons.android),
-              title: new Text('android'),
-            ),
-            new ListTile(
-              leading: new Icon(Icons.backup),
-              title: new Text('backup'),
-            ),
-            new Image.network("https://img1.mukewang.com/szimg/5c0493ce000126b106000338-360-202.jpg"),
-            new Image.network("https://img1.sycdn.imooc.com/szimg/5bc3f1420001abf206000338-360-202.jpg"),
-            new Image.network("https://climg.mukewang.com/5c8097e80001960406000338.jpg"),
-            new Image.network("https://img4.mukewang.com/szimg/59313618000198ed05400300-360-202.jpg"),
-            new Image.network("https://climg.mukewang.com/5c8097e80001960406000338.jpg")
-          ],
+        body: Center(
+          child: Container(
+            height: 200.0,
+            child: MyList()
+          ),
         )
       )
+    );
+  }
+}
+
+class MyList extends StatelessWidget {
+  Widget build (BuildContext context) {
+    return ListView(
+      scrollDirection: Axis.horizontal,
+      children: <Widget>[
+        new Container(
+          width: 180,
+          color: Colors.red,
+        ),
+        new Container(
+          width: 180,
+          color: Colors.greenAccent,
+        ),
+        new Container(
+          width: 180,
+          color: Colors.orange,
+        ),
+        new Container(
+          width: 180,
+          color: Colors.blue,
+        )
+      ],
     );
   }
 
