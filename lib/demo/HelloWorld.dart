0abc810436b1b487398fdffd5db124affc935d70
monkeycoderw <3897177+monkeycoderw@users.noreply.github.com>
2019-08-01 23:03:01 +0800

    containerWidget

--- lib/demo/HelloWorld.dart
+++ lib/demo/HelloWorld.dart
@@ -11,17 +11,30 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Text Widget'),
         ),
-        body: Center (
-          child: Text('国产首部IMAX动画电影《哪吒之魔童降世》成功引爆暑期档。黑眼圈、大板牙、塌鼻子，这个颠覆传统印象的“高燃哪吒”引发网民对“振兴国漫”的热烈讨论。',
-           textAlign: TextAlign.left,
-           style: TextStyle (
-             fontSize: 25,
-             color: Color.fromARGB(255, 255, 150, 150),
-             decoration: TextDecoration.underline,
-             decorationStyle: TextDecorationStyle.dotted
-           ),
-          ),
-
+        body: Center(
+          child: Container(
+            child: new Text(
+              'Hello Nick',
+              style: TextStyle(
+                fontSize: 40.0
+              ),
+            ),
+            alignment: Alignment.topCenter,
+            width: 300,
+            height: 400,
+//            color: Colors.grey,
+            padding: const EdgeInsets.fromLTRB(10,30,0,0),
+            margin:const EdgeInsets.all(5.0),
+            decoration: new BoxDecoration(
+              gradient: const LinearGradient(
+                colors: [
+                  Colors.lightBlue,
+                  Colors.greenAccent,
+                  Colors.purple
+                ]
+              ),
+            ),
+          )
         )
       )
     );
